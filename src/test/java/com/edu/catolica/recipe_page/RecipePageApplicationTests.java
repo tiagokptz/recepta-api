5f9508798711c48a589a994a76967aa288a33dbb
tiago <tiagoeliassilva2005@gmail.com>
2025-05-23 13:49:39 -0300

    criação do projeto spring

---
+++ src/test/java/com/edu/catolica/recipe_page/RecipePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.catolica.recipe_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
